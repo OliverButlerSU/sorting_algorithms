58de50184211a8e62b3f52ff1f024b861ac644c5
OliverButlerSU <ob3g21@soton.ac.uk>
2023-01-30 23:37:04 +0000

    Added Selection Sort

---
+++ selection sort/main.js
@@ -0,0 +1,53 @@
+window.onload = function() {
+    var canvas = document.getElementById("sortingCanvas");
+    var ctx = canvas.getContext("2d");
+
+    var sortingArray = [];
+
+    function initLines()
+    {
+        for (var i=0; i<250; i++)
+        {
+            sortingArray[i] = Math.floor(((Math.random())*500))
+        }
+    }
+
+    function drawLines(arr)
+    {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for (var i=0; i<arr.length; i++)
+        {
+            ctx.beginPath(); 
+            ctx.moveTo((i+1)*4+0.5, 0);
+            ctx.lineTo((i+1)*4+0.5, arr[i]);
+            ctx.stroke();
+            ctx.moveTo(0,0);
+        }
+    }
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async function selectionSort(arr)
+    {
+        for (var i=0; i < arr.length-1; i++){
+            var min = i
+            for(var j = i+1; j< arr.length; j++){
+                if(arr[j] < arr[min]){
+                    min = j
+                }
+            }
+            var temp = arr[min]
+            arr[min] = arr[i]
+            arr[i] = temp
+            drawLines(arr);
+            await sleep(10);
+        }
+    }
+
+
+    initLines();
+    drawLines(sortingArray);
+    selectionSort(sortingArray);
+}
