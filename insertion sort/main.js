48c00b76e63bd3568ff0754560dcd496f58ba487
OliverButlerSU <ob3g21@soton.ac.uk>
2023-01-31 00:35:56 +0000

    Fixed issue with 0 index with Insertion Sort

--- insertion sort/main.js
+++ insertion sort/main.js
@@ -34,7 +34,7 @@ window.onload = function() {
         for (var i=1; i < arr.length; i++){
             key = arr[i]
             j = i-1
-            while (arr[j] > key && j > 0){
+            while (arr[j] > key && j >= 0){
                 arr[j+1] = arr[j]
                 j--
             }
